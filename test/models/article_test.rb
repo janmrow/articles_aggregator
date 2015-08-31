41d010d7341c9c0b922e0bf4aaf50ab1b7277153
Jasiek <jasiek92@gmail.com>
2015-08-31 12:28:08 +0200

    add tests for article model

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @article = articles(:one)
+  end
+
+  test "correct article should be saved" do
+    assert @article.valid?
+  end
+
+  test "should not save article without tile" do
+    @article.title = ""
+    assert_not @article.valid?
+  end
+
+  test "should not save article without body" do
+    @article.body = ""
+    assert_not @article.valid?
+  end
+
+  test "title should have minimum 3 chars" do
+    @article.title = "a"*2
+    assert_not @article.valid?
+  end
+
+  test "body should have minimum 10 chars" do
+    @article.body = "a"*9
+    assert_not @article.valid?
+  end
+
 end
