955d816fa62c384ca3e4195c36f91f03a2d0ce8d
Jasiek <jasiek92@gmail.com>
2015-08-31 14:28:37 +0200

    add functional tests for article controller

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should get home page and render correct template" do
+    get :index
+    assert_response :success
+    assert_template :index
+    assert_not_nil assigns(:articles)
+    assert_select 'title', "Crud App"
+  end
+
+  test "should get new and render correct template" do
+    get :new
+    assert_response :success
+    assert_template :new
+    assert_select 'h2', "Add new article"
+  end
+
+  test "edit should render correct template" do
+    get :edit, id: articles(:one)
+    assert_response :success
+    assert_template :edit
+    assert_select 'h2', "Edit article"
+
+  end
+
+  test "should create article" do
+    assert_difference('Article.count') do
+      post :create, article: {title: "Test", body: "Test article."}
+    end
+    assert_redirected_to article_path(assigns(:article))
+  end
+
+  test "should delete article" do
+    assert_difference ['Article.count'], -1 do
+      delete :destroy, id: articles(:one)
+    end
+  end
+
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
+  
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "h2", "About"
   end
 
 end
