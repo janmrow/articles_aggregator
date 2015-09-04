f643ad4ea5696ccac9e741f898e21e5e83670569
Jasiek <jasiek92@gmail.com>
2015-09-04 21:38:08 +0200

    model tests fo comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
+  validates :author, presence: true, length: { minimum: 3 }
+  validates :body, presence: true, length: { minimum: 5 }
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @article = articles(:one)
+    @comment = @article.comments.create(author: "Tim", body: "Just a test.")
+  end
+
+  test "correct comment should be saved" do
+    assert @comment.valid?
+  end
+
+  test "should not save comment without author" do
+    @comment.author = ""
+    assert_not @comment.valid?
+  end
+
+  test "should not save comment without body" do
+    @comment.body = ""
+    assert_not @comment.valid?
+  end
+
+  test "author should have minimum 3 chars" do
+    @comment.author = "a"*2
+    assert_not @comment.valid?
+  end
+
+  test "body should have minimum 5 chars" do
+    @comment.body = "a"*4
+    assert_not @comment.valid?
+  end
+
 end
