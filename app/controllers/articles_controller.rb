0dc01506d9af905a7a5b21e56adde3e3bdb53395
Jasiek <jasiek92@gmail.com>
2015-09-05 17:30:59 +0200

    better sorting in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "admin", password: "admin", except: [ :index, :show ]
   
   def index
-    @articles = Article.all.reverse
+    @articles = Article.order("created_at DESC")
   end
 
   def show
